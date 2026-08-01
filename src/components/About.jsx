438876fe32134a7e2f31189ae70f2f2d0bdb6f91
DeejMags <rnmaglatangjr@gmail.com>
2026-08-01 08:35:49 +0800

    updated about me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ export default function About() {
       <p className="section__label">01 &mdash; about</p>
       <p>
         I'm a graduate of Bachelor of Science in Information Technology, majoring in the
-        Web Technology Track. I enjoy building full-stack web experiences &mdash; from crafting
+        Web Technology Track from University of the Cordilleras. I enjoy building full-stack web experiences &mdash; from crafting
         clean, responsive front-end interfaces to developing the back-end logic that powers them.
         I'm also comfortable designing in Figma and collaborating on projects through Git and
         GitHub, and I bring strong time-management and communication skills to every team I work with.
